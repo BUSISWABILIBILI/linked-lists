daaab0300f3d0871cdbbce7a3bf46986bd26c84e
Busiswa Bili-bili <141039125+BUSISWABILIBILI@users.noreply.github.com>
2026-05-22 11:52:47 +0200

    feat: implement at contains and findIndex methods

--- LinkedList.js
+++ LinkedList.js
@@ -31,11 +31,8 @@ class LinkedList {
   }
 
   size() {
-    if (this.headNode === null) {
-      return 0;
-    }
-
     let count = 0;
+
     let currentNode = this.headNode;
 
     while (currentNode !== null) {
@@ -64,12 +61,65 @@ class LinkedList {
     return currentNode.value;
   }
 
+  at(index) {
+    if (index < 0) {
+      return undefined;
+    }
+
+    let currentNode = this.headNode;
+
+    let currentIndex = 0;
+
+    while (currentNode !== null) {
+      if (currentIndex === index) {
+        return currentNode.value;
+      }
+
+      currentNode = currentNode.nextNode;
+      currentIndex++;
+    }
+
+    return undefined;
+  }
+
+  contains(value) {
+    let currentNode = this.headNode;
+
+    while (currentNode !== null) {
+      if (currentNode.value === value) {
+        return true;
+      }
+
+      currentNode = currentNode.nextNode;
+    }
+
+    return false;
+  }
+
+  findIndex(value) {
+    let currentNode = this.headNode;
+
+    let currentIndex = 0;
+
+    while (currentNode !== null) {
+      if (currentNode.value === value) {
+        return currentIndex;
+      }
+
+      currentNode = currentNode.nextNode;
+      currentIndex++;
+    }
+
+    return -1;
+  }
+
   toString() {
     if (this.headNode === null) {
       return "";
     }
 
     let currentNode = this.headNode;
+
     let result = "";
 
     while (currentNode !== null) {
--- main.js
+++ main.js
@@ -2,13 +2,19 @@ import LinkedList from "./LinkedList.js";
 
 const list = new LinkedList();
 
+list.append("dog");
 list.append("cat");
 list.append("parrot");
-
-list.prepend("dog");
+list.append("hamster");
 
 console.log(list.toString());
 
-console.log("Size:", list.size());
-console.log("Head:", list.head());
-console.log("Tail:", list.tail());
+console.log("At index 2:", list.at(2));
+
+console.log("Contains cat:", list.contains("cat"));
+
+console.log("Contains snake:", list.contains("snake"));
+
+console.log("Find parrot:", list.findIndex("parrot"));
+
+console.log("Find turtle:", list.findIndex("turtle"));
