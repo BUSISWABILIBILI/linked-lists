8216770e0a8d72c6cc0cfacda6b96b70dcffab1b
Busiswa Bili-bili <141039125+BUSISWABILIBILI@users.noreply.github.com>
2026-05-22 12:01:48 +0200

    docs: finalize linked list project

---
+++ README.md
@@ -0,0 +1,24 @@
+# Linked Lists
+
+A linked list data structure built with JavaScript for The Odin Project.
+
+## Features
+
+- Append nodes
+- Prepend nodes
+- Remove nodes
+- Insert nodes at specific indexes
+- Search linked list values
+- Traverse nodes
+- Convert linked list to string representation
+
+## Built With
+
+- JavaScript
+- Node.js
+
+## Run Project
+
+```bash
+node main.js
+```
--- main.js
+++ main.js
@@ -6,9 +6,31 @@ list.append("dog");
 list.append("cat");
 list.append("parrot");
 list.append("hamster");
+list.append("snake");
+list.append("turtle");
 
 console.log(list.toString());
 
-console.log("Removed:", list.removeAt(2));
+console.log("Size:", list.size());
+
+console.log("Head:", list.head());
+
+console.log("Tail:", list.tail());
+
+console.log("At index 2:", list.at(2));
+
+console.log("Contains hamster:", list.contains("hamster"));
+
+console.log("Find snake:", list.findIndex("snake"));
+
+console.log("Pop:", list.pop());
+
+console.log(list.toString());
+
+list.insertAt(2, "lion", "elephant");
+
+console.log(list.toString());
+
+console.log("Removed:", list.removeAt(3));
 
 console.log(list.toString());
