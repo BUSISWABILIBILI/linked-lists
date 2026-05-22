23b49fc1ff8a3ddb83bc043afe85e0de2404b2c8
Busiswa Bili-bili <141039125+BUSISWABILIBILI@users.noreply.github.com>
2026-05-22 11:51:23 +0200

    feat: implement prepend size head and tail methods

--- LinkedList.js
+++ LinkedList.js
@@ -22,6 +22,48 @@ class LinkedList {
     currentNode.nextNode = newNode;
   }
 
+  prepend(value) {
+    const newNode = new Node(value);
+
+    newNode.nextNode = this.headNode;
+
+    this.headNode = newNode;
+  }
+
+  size() {
+    if (this.headNode === null) {
+      return 0;
+    }
+
+    let count = 0;
+    let currentNode = this.headNode;
+
+    while (currentNode !== null) {
+      count++;
+      currentNode = currentNode.nextNode;
+    }
+
+    return count;
+  }
+
+  head() {
+    return this.headNode?.value;
+  }
+
+  tail() {
+    if (this.headNode === null) {
+      return undefined;
+    }
+
+    let currentNode = this.headNode;
+
+    while (currentNode.nextNode !== null) {
+      currentNode = currentNode.nextNode;
+    }
+
+    return currentNode.value;
+  }
+
   toString() {
     if (this.headNode === null) {
       return "";
--- main.js
+++ main.js
@@ -2,8 +2,13 @@ import LinkedList from "./LinkedList.js";
 
 const list = new LinkedList();
 
-list.append("dog");
 list.append("cat");
 list.append("parrot");
 
+list.prepend("dog");
+
 console.log(list.toString());
+
+console.log("Size:", list.size());
+console.log("Head:", list.head());
+console.log("Tail:", list.tail());
